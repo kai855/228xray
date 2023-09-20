24fe1bc833f09069fe0dacc8c70adad8e40032fa
kai855 <54061535+kai855@users.noreply.github.com>
2023-09-20 19:25:21 +0800

    Update 节点订阅.sh

--- xray/节点订阅.sh
+++ xray/节点订阅.sh
@@ -77,7 +77,7 @@ url1=$(awk -F'url1=' '/url1/{print $2; exit}'  /data/adb/modules/xray/url.txt)
 url2=$(awk -F'url2=' '/url2/{print $2; exit}'  /data/adb/modules/xray/url.txt)
 url3=$(awk -F'url3=' '/url3/{print $2; exit}'  /data/adb/modules/xray/url.txt)
 url4=$(awk -F'url4=' '/url4/{print $2; exit}'  /data/adb/modules/xray/url.txt)
-url5=$(awk -F'url4=' '/url5/{print $2; exit}'  /data/adb/modules/xray/url.txt)
+url5=$(awk -F'url5=' '/url5/{print $2; exit}'  /data/adb/modules/xray/url.txt)
 
 case $url in
     1)
