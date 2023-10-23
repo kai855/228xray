ad321c156904d0f73eda6bcc2c3569d08e4cc9b3
kai855 <54061535+kai855@users.noreply.github.com>
2023-10-23 21:07:56 +0800

    Update 延迟测试.sh

--- xray/延迟测试.sh
+++ xray/延迟测试.sh
@@ -32,7 +32,7 @@ plain='\033[0m'
 proxy=$(awk -F'=' '/proxy/{print $2; exit}' /data/xray/xray设置.txt)
 
 #国内节点匹配正则
-node_pmatching="移动|电信|联通|China|空配|🇨🇳"
+node_pmatching="移动|电信|联通|China|空配|🇨🇳|内蒙"
 
 
 
